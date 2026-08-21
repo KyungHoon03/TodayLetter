c87dcb48967b93a56874d7030789f15a7b98578c
KyungHoon03 <tw21lkh@naver.com>
2026-08-21 22:47:45 +0900

    [set] first setting

---
+++ src/main/java/com/todayletter/todayletter/TodayLetterApplication.java
@@ -0,0 +1,13 @@
+package com.todayletter.todayletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayLetterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodayLetterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/todayletter/todayletter/TodayLetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todayletter.todayletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayLetterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
